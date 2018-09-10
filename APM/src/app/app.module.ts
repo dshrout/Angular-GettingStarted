3e009604b4925b823a8b5a7375c44639226a0382
Don Shrout <dshrout@gmail.com>
2018-09-10 06:50:05 -0500

    Working Project

---
+++ APM/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces';
+import { StarComponent } from './shared/star.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule 
+  ],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ APM/src/app/products/product.ts
@@ -0,0 +1,11 @@
+
+export interface IProduct {
+    productId: number,
+    productName: string,
+    productCode: string,
+    releaseDate: string,
+    price: number,
+    starRating: number,
+    description: string,
+    imageUrl: string
+}
